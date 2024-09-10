754fbe44ca3067e0aa7b68f14a462ecc11f80cc8
danilPorkchop15 <danilkostin150705@gmail.com>
2024-09-10 10:15:07 +0300

    feat(course): add spy task

---
+++ course/6_spy.js
@@ -0,0 +1,12 @@
+// Создайте декоратор spy(func), который должен возвращать обёртку, 
+// которая сохраняет все вызовы функции в своём свойстве calls.
+// Каждый вызов должен сохраняться как массив аргументов.
+
+function spy(func) {
+  function wrapper(...args) {
+    wrapper.calls.push(args);
+    return func.call(this, ...args);
+  }
+  wrapper.calls = [];
+  return wrapper;
+}
