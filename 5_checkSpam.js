7804befed217ec7c11ddfd58d92e646dee54f5e0
danilPorkchop15 <danilkostin150705@gmail.com>
2024-09-10 10:12:54 +0300

    feat(course): add factorial task

--- 5_checkSpam.js
+++
@@ -1,5 +0,0 @@
-// Напишите функцию checkSpam(str), возвращающую true, 
-// если str содержит 'viagra' или 'XXX', а иначе false.
-function checkSpam(str) {
-  return ["xxx", "viagra"].some((el) => str.toLowerCase().includes(el));
-}
