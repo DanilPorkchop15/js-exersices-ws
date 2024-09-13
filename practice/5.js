eaf83244cb8a9fbcd0f0353c59629d1708f8d4e2
danilPorkchop15 <danilkostin150705@gmail.com>
2024-09-13 11:51:01 +0300

    feat(prectice): add task 5 (pipe function)

---
+++ practice/5.js
@@ -0,0 +1,26 @@
+// 5) Реализуйте функцию pipe. Она должна принимать неопределенное количество функций-обработчиков и возвращать функцию, 
+// которая будет прогонять принимаемый параметр через все обработчики, а на выходе вернет результат работы. 
+// Функции-обработчики должны вызываться справа налево.
+
+// const fillUser = pipe (
+//   (user) => ({ ...user, lastName: "Pass" }),
+//   (user) => ({ ...user, age: 29 }),
+//   (user) => ({ ...user, city: "Boston" }),
+// );
+
+function pipe () {
+  const funcs = [...arguments];
+  console.log(funcs)
+  return function (payload) {
+    return funcs.reduce((acc, func) => acc = func(acc), payload ? payload : {})
+  }
+}
+
+const fillUser = pipe (
+  (user) => ({ ...user, lastName: "Pass" }),
+  (user) => ({ ...user, age: 29 }),
+  (user) => ({ ...user, city: "Boston" }),
+);
+
+console.log(fillUser({a : 123}))
+console.log(fillUser())
