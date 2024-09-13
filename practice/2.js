44cee8f18c3dad685fbb7ac11de91cbac5d253e8
danilPorkchop15 <danilkostin150705@gmail.com>
2024-09-13 09:46:18 +0300

    feat(prectice): add task 2 (shallowEquals)

---
+++ practice/2.js
@@ -0,0 +1,23 @@
+// 2) Напишите функцию для поверхностного сравнения двух объектов. Поверхностное сравнение объектов - 
+// это процесс сравнения ключей объекта и их значений, без учета уровня вложенности. 
+// То есть значение с типом object, никогда не будет равен точно такому же значению с типом object, т.к. ссылки на эти значения будут отличаться.
+// Пример работы функции сравнения:
+
+// shallowEquals({ a: 1, b: "2" }, { a: 1, b: "2" }); // true
+// shallowEquals({ a: 0 }, { a: undefined }); // false
+// shallowEquals({ a: {} }, { a: {} }); // false
+// shallowEquals({ a: [] }, { a: [] }); // false
+// shallowEquals({ a: () => {} }, { a: () => {} }); // false
+
+function shallowEquals(obj1, obj2) {
+  for (const key in obj1) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+  return true;
+}
+
+console.log(shallowEquals({ a: 1, b: "2"}, { a: 1, b: "2" })); 
+console.log(shallowEquals({ a: 0 }, { a: undefined }));
+console.log(shallowEquals({ a: {} }, { a: {} }));
+console.log(shallowEquals({ a: [] }, { a: [] }));
+console.log(shallowEquals({ a: () => {} }, { a: () => {} }));
