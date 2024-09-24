212ba9116507de29dfcf540380c904df058da292
Danil Kostin <danilkostin150705@gmail.com>
2024-09-24 09:02:51 +0300

    feat(course): groupById  task modified

--- course/5 Типы данных/5_groubById.js
+++ course/5 Типы данных/5_groubById.js
@@ -3,8 +3,30 @@
 // ключа и элементами массива в качестве значений.
 
 function groupById(arr) {
-  return arr.reduce ((res, el) => {
-  res [el.id] = el;
-  return res;
+  return arr.reduce ((res, {id, ...rest}) => {
+  res [id] = rest;
+  return res
   }, {});
 }
+
+function groupById2(arr) {
+  let result = {};
+  for (let {id, ...rest} of arr) result[id] = rest;
+  return result;
+}
+
+console.log(
+  groupById([
+    {id: '1', name: "John Smith", age: 20},
+    {id: '2', name: "Ann Smith", age: 24},
+    {id: '3', name: "Pete Peterson", age: 31},
+  ])
+)
+
+console.log(
+  groupById2([
+    {id: '1', name: "John Smith", age: 20},
+    {id: '2', name: "Ann Smith", age: 24},
+    {id: '3', name: "Pete Peterson", age: 31},
+  ])
+)
